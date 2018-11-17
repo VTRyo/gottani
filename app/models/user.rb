e3d11ac4a45a8a07bdf1eab2b4b53e635e1687bf
Ryo inaba <vtryo.work@gmail.com>
2018-11-17 16:30:19 +0900

    user model done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,49 @@ class User < ApplicationRecord
     has_many :users_of_sending_claps_to, through: :active_claps, source: :to_user
 
     def to_param
-        self.niclname
+        self.nickname
+    end
+
+    # 拍手する相手Userを受け取り、Clapレコードを生成するメソッド
+    def clap(user)
+        Clap.create(from_user_id: self.id, to_user_id: user&.id)
+    end
+
+    # 拍手したことをつぶやくためのメソッド
+    def tweet(text)
+        client = Twitter::REST::Client.new do |config|
+            config.customer_key         = ENV['API_KEY']
+            config.customer_secret      = ENV['API_SECRET']
+            config.access_token         = token
+            config.access_token_secret  = secret
+        end
+        client.update(text)
+    end
+
+    # ログイン情報を保持するあためのTokenを生成するクラスメソッド
+    def self.new_remember_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # self.new_remember_tokenを暗号化するためのクラスメソッド
+    def self.encrypt(token)
+        Diget::SHA256.hexdigest(token.to_s)
+    end
+
+    # Twitterログインやユーザ作成を行うためのメソッド。今回はTwitter Oauth認証時のコールバック時に呼び出される
+    def self.find_or_create_with_omniauth(auth, remember_token)
+        provider = auth['provider']
+        uid      = auth['uid']
+
+        user = User.find_or_create_by(provider: provider, uid: uid) do |user|
+            user.name       = auth['info']['name']
+            user.nickname   = auth['info']['nickname']
+            user.image_url  = auth['info']['image']
+            user.token      = auth['credentials']['token']
+            user.secret     = auth['credentials']['secret']
+        end
+
+        user.update!(remember_token: encrypt(remember_token))
+    end
     end
 end
